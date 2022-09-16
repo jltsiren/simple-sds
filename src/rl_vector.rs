4e84bcec76ec96eb4999cc13cff544130c427c3e
Jouni Siren <jouni.siren@iki.fi>
2022-09-16 07:51:40 -0700

    Started working on RLVector

--- src/rl_vector.rs
+++ src/rl_vector.rs
@@ -1,5 +1,6 @@
 // FIXME document
 
+use crate::int_vector::IntVector;
 
 // FIXME tests
 //#[cfg(test)]
@@ -7,15 +8,79 @@
 
 //-----------------------------------------------------------------------------
 
-// FIXME RLVector
+// FIXME document, example, tests
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct RLVector {
+    len: usize,
+    ones: usize,
+    // FIXME divisors for using rank/select indexes
+    rank_index: IntVector,
+    select_index: IntVector,
+    // (bits, ones) until the start of each block.
+    samples: IntVector,
+    // Concatenated blocks.
+    data: IntVector,
+}
 
 // FIXME impl
+impl RLVector {
+    /// Number of bits in a code unit.
+    pub const CODE_SIZE: usize = 4;
+
+    /// Number of code units in a block.
+    pub const BLOCK_SIZE: usize = 64;
+}
 
 //-----------------------------------------------------------------------------
 
-// FIXME RLBuilder
+// FIXME document, example, tests
+#[derive(Clone, Debug)]
+pub struct RLBuilder {
+    len: usize,
+    ones: usize,
+    // Position after the last run.
+    tail: usize,
+    run: (usize, usize),
+    samples: Vec<(usize, usize)>,
+    data: IntVector,
+}
 
-// FIXME impl
+// FIXME document
+impl RLBuilder {
+    pub fn new() -> Self {
+        RLBuilder {
+            len: 0,
+            ones: 0,
+            tail: 0,
+            run: (0, 0),
+            samples: Vec::new(),
+            data: IntVector::new(RLVector::CODE_SIZE).unwrap(),
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    pub fn count_ones(&self) -> usize {
+        self.ones
+    }
+
+    pub fn tail(&self) -> usize {
+        self.tail
+    }
+
+    // FIXME set
+    // FIXME set_run
+    // FIXME unchecked versions?
+    // FIXME set_len
+
+    // FIXME flush
+}
 
 // FIXME extend
 
@@ -69,7 +134,7 @@
 
 //-----------------------------------------------------------------------------
 
-// FIXME From BitVec + Select
+// FIXME From other bitvector types
 
 // FIXME TryFrom RLBuilder
 
