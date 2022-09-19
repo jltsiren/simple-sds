53be113b07c9b438625d6efc0e49759cd0e80c80
Jouni Siren <jouni.siren@iki.fi>
2022-09-19 15:01:06 -0700

    Some work on RLVector

--- src/rl_vector.rs
+++ src/rl_vector.rs
@@ -1,9 +1,30 @@
-// FIXME document
+//! A run-length encoded bitvector supporting rank, select, and related queries.
+//!
+//! This is a run-length encoded bitvector similar to the one used in RLCSA:
+//!
+//! > Mäkinen, Navarro, Sirén, Välimäki: Storage and Retrieval of Highly Repetitive Sequence Collections.  
+//! > Journal of Computational Biology, 2010.  
+//! > DOI: [10.1089/cmb.2009.0169](https://doi.org/10.1089/cmb.2009.0169)
+//!
+//! The vector is encoded by storing the maximal runs of unset and set bits.
+//! If there is a run of `n0` unset bits followed by a run of `n1` set bits, we encode it as integers `n0` and `n1 - 1`.
+//! Each integer is encoded in little-endian order using 4-bit code units.
+//! The lowest 3 bits of each code unit contain data.
+//! If the high bit is set, the encoding continues in the next unit.
+//! We partition the encoding into 64-unit (32-byte) blocks that consist of entire runs.
+//! If there is not enough space left for encoding the next `(n0, n1)`, we pad the block with empty code units and start a new block.
+//!
+//! For each block, we store a sample `(i, rank(i, 1))`, where `i` is the number of bits encoded before that block.
+//! Queries use binary search on the samples to find the right block and then decompress the block sequentially.
+// FIXME rank/select indexes
+
 // FIXME document serialization format
 
 use crate::int_vector::IntVector;
-use crate::ops::{Vector, Push, Resize, BitVec, Select};
+use crate::ops::{Vector, Access, Push, Resize, BitVec, Select};
 use crate::bits;
+
+use std::iter::FusedIterator;
 
 // FIXME the rank/select index structure as a submodule
 
@@ -25,7 +46,6 @@ pub struct RLVector {
     data: IntVector,
 }
 
-// FIXME copy_bit_vec
 impl RLVector {
     /// Number of bits in a code unit.
     pub const CODE_SIZE: usize = 4;
@@ -54,6 +74,89 @@ impl RLVector {
         }
         builder.set_len(source.len());
         RLVector::from(builder)
+    }
+
+    // FIXME document
+    pub fn run_iter(&self) -> RunIter<'_> {
+        RunIter {
+            parent: self,
+            offset: 0,
+            pos: (0, 0),
+            limit: self.ones_after(0),
+        }
+    }
+
+    // Returns the number of blocks in the encoding.
+    fn blocks(&self) -> usize {
+        self.samples.len() / 2
+    }
+
+    // Returns the number of set bits after the given block.
+    fn ones_after(&self, block: usize) -> usize {
+        if block + 1 < self.blocks() {
+            self.samples.get(2 * (block + 1) + 1) as usize
+        } else {
+            self.count_ones()
+        }
+    }
+
+    // Decodes a value starting from `offset` and returns (value, new offset).
+    fn decode(&self, offset: usize) -> (usize, usize) {
+        let mut value = 0;
+        let mut offset = offset;
+        let mut shift = 0;
+        loop {
+            let code = self.data.get(offset);
+            offset += 1;
+            value += ((code & Self::CODE_MASK) << shift) as usize;
+            shift += Self::CODE_SHIFT;
+            if code & Self::CODE_FLAG == 0 {
+                return (value, offset);
+            }
+        }
+    }
+
+    // Returns the identifier of the last block `i` in the range where `f(i) < value`.
+    fn block_for<F: Fn(usize) -> usize>(low: usize, high: usize, value: usize, f: F) -> usize {
+        let mut low = low;
+        let mut high = high;
+        while high - low > 1 {
+            let mid = low + (high - low) / 2;
+            let candidate = f(mid);
+            if candidate < value {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+        low
+    }
+
+    // Returns the identifier of the block containing the given bit.
+    fn block_for_bit(&self, index: usize) -> Option<usize> {
+        if index >= self.len() {
+            return None;
+        }
+        // FIXME use the index
+        Some(Self::block_for(0, self.blocks(), index, |i| self.samples.get(2 * i) as usize))
+    }
+
+    // Returns the identifier of the block containing the set bit of given rank.
+    fn block_for_one(&self, rank: usize) -> Option<usize> {
+        if rank >= self.count_ones() {
+            return None;
+        }
+        // FIXME use the index
+        Some(Self::block_for(0, self.blocks(), rank, |i| self.samples.get(2 * i + 1) as usize))
+    }
+
+    // Returns the indentifier of the block containing the unset bit of given rank.
+    fn block_for_zero(&self, rank: usize) -> Option<usize> {
+        if rank >= self.count_zeros() {
+            return None;
+        }
+        // FIXME use the index
+        Some(Self::block_for(0, self.blocks(), rank, |i| (self.samples.get(2 * i) - self.samples.get(2 * i + 1)) as usize))
     }
 }
 
@@ -243,19 +346,134 @@ impl From<RLBuilder> for RLVector {
 
 //-----------------------------------------------------------------------------
 
-// FIXME RunIter
+// FIXME document, example, tests
+pub struct RunIter<'a> {
+    parent: &'a RLVector,
+    // Offset in the encoding.
+    offset: usize,
+    // (bitvector offset, number of ones)
+    pos: (usize, usize),
+    // Number of ones after the current block.
+    limit: usize,
+}
 
-// FIXME Iterator, ExactSizeIterator, FusedIterator
+impl<'a> Iterator for RunIter<'a> {
+    // (start, length)
+    type Item = (usize, usize);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.offset >= self.parent.data.len() {
+            return None;
+        }
+
+        // Move to the next block if we have run out of ones.
+        if self.pos.1 > self.limit {
+            let block = bits::div_round_up(self.offset, RLVector::BLOCK_SIZE);
+            self.offset = block * RLVector::BLOCK_SIZE;
+            if block >= self.parent.blocks() {
+                return None;
+            }
+            self.limit = self.parent.ones_after(block);
+        }
+
+        // Decode the next run.
+        let (gap, next_offset) = self.parent.decode(self.offset);
+        let start = self.pos.0 + gap;
+        let (len, next_offset) = self.parent.decode(next_offset);
+        self.offset = next_offset;
+        self.pos.0 = start + len + 1;
+        self.pos.1 += len + 1;
+
+        Some((start, len + 1))
+    }
+}
+
+impl<'a> FusedIterator for RunIter<'a> {}
 
 //-----------------------------------------------------------------------------
 
-// FIXME Iter
+// FIXME document, example, tests
+pub struct Iter<'a> {
+    iter: RunIter<'a>,
+    // Run from the iterator.
+    run: Option<(usize, usize)>,
+    // Bitvector offset.
+    pos: usize,
+}
 
-// FIXME Iterator, ExactSizeIterator, FusedIterator
+impl<'a> Iterator for Iter<'a> {
+    type Item = bool;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // Read the next run if we have processed the current one.
+        if self.run.is_some() {
+            let (start, len) = self.run.unwrap();
+            if self.pos >= start + len {
+                self.run = self.iter.next();
+            }
+        }
+
+        // Determine the next bit and advance.
+        self.pos += 1;
+        match self.run {
+            Some((start, _)) => Some(self.pos > start),
+            None => if self.pos > self.iter.parent.len() { None } else { Some(false) },
+        }
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let remaining = self.iter.parent.len() - self.pos;
+        (remaining, Some(remaining))
+    }
+}
+
+impl<'a> FusedIterator for Iter<'a> {}
 
 //-----------------------------------------------------------------------------
 
-// FIXME BitVec
+impl<'a> BitVec<'a> for RLVector {
+    type Iter = Iter<'a>;
+
+    #[inline]
+    fn len(&self) -> usize {
+        self.len
+    }
+
+    #[inline]
+    fn count_ones(&self) -> usize {
+        self.ones
+    }
+
+    fn get(&self, index: usize) -> bool {
+        let block = self.block_for_bit(index).unwrap();
+        let mut iter = RunIter {
+            parent: self,
+            offset: block * Self::BLOCK_SIZE,
+            pos: (self.samples.get(2 * block) as usize, self.samples.get(2 * block + 1) as usize),
+            limit: self.ones_after(block),
+        };
+
+        while let Some((start, len)) = iter.next() {
+            if start > index {
+                return false;
+            }
+            if index < start + len {
+                return true;
+            }
+        }
+
+        panic!("RLVector::get(): Cannot find bit {}", index);
+    }
+
+    fn iter(&'a self) -> Self::Iter {
+        Self::Iter {
+            iter: self.run_iter(),
+            run: Some((0, 0)),
+            pos: 0,
+        }
+    }
+}
 
 //-----------------------------------------------------------------------------
 
@@ -292,7 +510,5 @@ impl From<RLBuilder> for RLVector {
 //-----------------------------------------------------------------------------
 
 // FIXME From other bitvector types
-
-// FIXME TryFrom RLBuilder
 
 //-----------------------------------------------------------------------------
