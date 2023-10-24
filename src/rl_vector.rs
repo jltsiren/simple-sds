f7dd8fa7fbd5fa1601dcdde098be77e87fc5f735
Jouni Siren <jouni.siren@iki.fi>
2023-10-23 21:22:04 -0700

    RLVector in benchmarks

--- src/rl_vector.rs
+++ src/rl_vector.rs
@@ -18,8 +18,6 @@
 //! Queries use binary search on the samples to find the right block and then decompress the block sequentially.
 //! A [`SampleIndex`] is used for narrowing down the range of the binary search.
 
-// FIXME use in benchmarks
-
 use crate::int_vector::IntVector;
 use crate::ops::{Vector, Access, Push, Resize, BitVec, Rank, Select, SelectZero, PredSucc};
 use crate::rl_vector::index::SampleIndex;
